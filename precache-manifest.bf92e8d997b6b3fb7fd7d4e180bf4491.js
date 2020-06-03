de70b3090b23a218bfe6d6bc4a2f7c75cbfe627a
mann-bansal <55892136+mann-bansal@users.noreply.github.com>
2020-06-03 19:02:02 +0530

    Updates

---
+++ precache-manifest.bf92e8d997b6b3fb7fd7d4e180bf4491.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abe9451f353126416ff70973aae7a4af",
+    "url": "/mann-bansal/contactmanager/index.html"
+  },
+  {
+    "revision": "fe4812d460ec3283d4f4",
+    "url": "/mann-bansal/contactmanager/static/css/2.4819d237.chunk.css"
+  },
+  {
+    "revision": "44a3d1f2700dab4cc086",
+    "url": "/mann-bansal/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "fe4812d460ec3283d4f4",
+    "url": "/mann-bansal/contactmanager/static/js/2.a3e20dff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/mann-bansal/contactmanager/static/js/2.a3e20dff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44a3d1f2700dab4cc086",
+    "url": "/mann-bansal/contactmanager/static/js/main.193a15dd.chunk.js"
+  },
+  {
+    "revision": "0ac58abde074eeb68743",
+    "url": "/mann-bansal/contactmanager/static/js/runtime-main.d1e18b0a.js"
+  }
+]);
